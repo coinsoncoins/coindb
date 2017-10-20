476938eb69c9284ee54435a89596bdbc01261304
Coins On Coins <coinsoncoins00@gmail.com>
2017-10-20 10:39:46 -0500

    add db

--- app.rb
+++ app.rb
@@ -3,4 +3,7 @@ require 'json'
 require 'pry'
 file = File.read('data/cmc.json')
 data = JSON.parse(file)
-binding.pry
+
+File.open("db/db.json","w") do |f|
+  f.write(JSON.pretty_generate(data))
+end
