8211ea8f1c84a851d88c8140a3d439ae2bea64ed
Coins On Coins <coinsoncoins00@gmail.com>
2017-10-20 18:38:37 -0500

    sort by market cap

--- app/queries.rb
+++ app/queries.rb
@@ -9,10 +9,11 @@ class Queries
   def cryptos_listed_by_month(date)
     filtered_cryptos = @cryptos.select do |coin| 
       if coin['date_added']
-        Date.parse(coin['date_added']).month == date.month 
+        date_added = Date.parse(coin['date_added'])
+        date_added.month == date.month && date_added.year == date.year
       end
     end
-    filtered_cryptos.sort_by! { |c| Date.parse(c['date_added']).day }
+    filtered_cryptos.sort_by! { |c| c['market_cap_usd'].to_f }
     filtered_cryptos
   end
 end
@@ -23,9 +24,9 @@ if __FILE__ == $0
   file = File.read('db/db.json')
   cryptos = JSON.parse(file)
   q = Queries.new(cryptos)
-  filtered_cryptos = q.cryptos_listed_by_month(Date.parse("2017/08/01"))
+  filtered_cryptos = q.cryptos_listed_by_month(Date.parse("2017/07/01"))
   filtered_cryptos.each do |c|
-    puts "#{c['id']}: #{c['date_added']}"
+    puts "#{c['id']}: marketcap: #{c['market_cap_usd']}, date_added: #{c['date_added']}"
   end
 end
 
